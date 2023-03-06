ed79759d94c64288e8bbebab122635e619032a15
sad1e <xing.ouyang.work@gmail.com>
2023-03-06 22:20:51 +0800

    add nvrhi.

---
+++ donut/nvrhi/rtxmu/include/rtxmu/D3D12Suballocator.h
@@ -0,0 +1,149 @@
+/*
+* Copyright (c) 2021 NVIDIA CORPORATION. All rights reserved
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#pragma once
+
+#include "Suballocator.h"
+#include <d3d12.h>
+
+#pragma comment(lib, "D3D12.lib")
+
+namespace rtxmu
+{
+    // If suballocator blocks are larger than 4 MB then use placed resources for large page sizes
+    constexpr bool Use4MBAlignedPlacedResources = true;
+
+    class D3D12Block
+    {
+    public:
+
+        static D3D12_GPU_VIRTUAL_ADDRESS getGPUVA(D3D12Block block,
+                                                  uint64_t   offset);
+
+        void allocate(ID3D12Device5*        device,
+                      uint64_t              size,
+                      D3D12_HEAP_TYPE       heapType,
+                      D3D12_RESOURCE_STATES state,
+                      uint32_t              alignment);
+
+        void free(ID3D12Device5* device);
+
+        uint64_t getVMA();
+
+        ID3D12Resource* getResource();
+
+    private:
+
+        ID3D12Resource* m_resource = nullptr;
+
+    };
+
+    class D3D12ScratchBlock : public D3D12Block
+    {
+    public:
+        static constexpr D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+        static constexpr D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_DEFAULT;
+        static constexpr uint32_t alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+
+        uint32_t getAlignment() { return alignment; }
+
+        void allocate(ID3D12Device5* device, uint64_t size)
+        {
+            D3D12Block::allocate(device, size, heapType, state, alignment);
+        }
+    };
+
+    class D3D12AccelStructBlock : public D3D12Block
+    {
+    public:
+        static constexpr D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_RAYTRACING_ACCELERATION_STRUCTURE;
+        static constexpr D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_DEFAULT;
+        static constexpr uint32_t alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+
+        uint32_t getAlignment() { return alignment; }
+
+        void allocate(ID3D12Device5* device, uint64_t size)
+        {
+            D3D12Block::allocate(device, size, heapType, state, alignment);
+        }
+    };
+
+    class D3D12ReadBackBlock : public D3D12Block
+    {
+    public:
+        static constexpr D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_COPY_DEST;
+        static constexpr D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_READBACK;
+        static constexpr uint32_t alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+
+        uint32_t getAlignment() { return alignment; }
+
+        void allocate(ID3D12Device5* device, uint64_t size)
+        {
+            D3D12Block::allocate(device, size, heapType, state, alignment);
+        }
+    };
+
+    class D3D12CompactionWriteBlock : public D3D12Block
+    {
+    public:
+        static constexpr D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+        static constexpr D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_DEFAULT;
+        static constexpr uint32_t alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+
+        uint32_t getAlignment() { return alignment; }
+
+        void allocate(ID3D12Device5* device, uint64_t size)
+        {
+            D3D12Block::allocate(device, size, heapType, state, alignment);
+        }
+    };
+
+    class D3D12UploadCPUBlock : public D3D12Block
+    {
+    public:
+        static constexpr D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_GENERIC_READ;
+        static constexpr D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_UPLOAD;
+        static constexpr uint32_t alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+
+        uint32_t getAlignment() { return alignment; }
+
+        void allocate(ID3D12Device5* device, uint64_t size)
+        {
+            D3D12Block::allocate(device, size, heapType, state, alignment);
+        }
+    };
+
+    class D3D12UploadGPUBlock : public D3D12Block
+    {
+    public:
+        static constexpr D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_COPY_DEST;
+        static constexpr D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_DEFAULT;
+        static constexpr uint32_t alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+
+        uint32_t getAlignment() { return alignment; }
+
+        void allocate(ID3D12Device5* device, uint64_t size)
+        {
+            D3D12Block::allocate(device, size, heapType, state, alignment);
+        }
+    };
+}
---
+++ donut/nvrhi/rtxmu/src/VulkanSuballocator.cpp
@@ -0,0 +1,110 @@
+/*
+* Copyright (c) 2021 NVIDIA CORPORATION. All rights reserved
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#include <rtxmu/VulkanSuballocator.h>
+
+namespace rtxmu
+{
+    vk::DispatchLoaderDynamic VkBlock::m_dispatchLoader;
+
+    vk::DispatchLoaderDynamic& VkBlock::getDispatchLoader()
+    {
+        return m_dispatchLoader;
+    }
+
+    uint32_t VkBlock::getMemoryIndex(const vk::PhysicalDevice& physicalDevice,
+                                     uint32_t memoryTypeBits,
+                                     vk::MemoryPropertyFlags propFlags,
+                                     vk::MemoryHeapFlags heapFlags)
+    {
+        vk::PhysicalDeviceMemoryProperties memProperties;
+        physicalDevice.getMemoryProperties(&memProperties, m_dispatchLoader);
+
+        for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++)
+        {
+            vk::MemoryType* memoryType = &memProperties.memoryTypes[i];
+            if ((memoryTypeBits & (1 << i)) &&
+                (memoryType->propertyFlags & propFlags) &&
+                (memProperties.memoryHeaps[memoryType->heapIndex].flags & heapFlags))
+            {
+                return i;
+
+            }
+        }
+        return 0;
+    }
+
+    vk::DeviceMemory VkBlock::getMemory(VkBlock block)
+    {
+        return block.m_memory;
+    }
+
+    vk::DeviceAddress VkBlock::getDeviceAddress(const vk::Device& device,
+                                               VkBlock block,
+                                               uint64_t offset)
+    {
+        auto addrInfo = vk::BufferDeviceAddressInfo().setBuffer(block.m_buffer);
+
+        return device.getBufferAddress(addrInfo, VkBlock::getDispatchLoader()) + offset;
+    }
+
+    vk::Buffer VkBlock::getBuffer()
+    {
+        return m_buffer;
+    }
+
+    void VkBlock::allocate(Allocator*              allocator,
+                           vk::DeviceSize          size,
+                           vk::BufferUsageFlags    usageFlags,
+                           vk::MemoryPropertyFlags propFlags,
+                           vk::MemoryHeapFlags     heapflags)
+    {
+        auto bufferInfo = vk::BufferCreateInfo()
+            .setSize(size)
+            .setUsage(usageFlags)
+            .setSharingMode(vk::SharingMode::eExclusive);
+
+        m_buffer = allocator->device.createBuffer(bufferInfo, nullptr, VkBlock::getDispatchLoader());
+
+        vk::MemoryRequirements memoryRequirements = allocator->device.getBufferMemoryRequirements(m_buffer, m_dispatchLoader);
+        uint32_t memoryTypeIndex = getMemoryIndex(allocator->physicalDevice, memoryRequirements.memoryTypeBits, propFlags, heapflags);
+
+        auto memoryAllocateFlags = vk::MemoryAllocateFlagsInfo()
+            .setFlags(vk::MemoryAllocateFlagBits::eDeviceAddress);
+
+        auto memoryInfo = vk::MemoryAllocateInfo()
+            .setPNext(&memoryAllocateFlags)
+            .setAllocationSize(size)
+            .setMemoryTypeIndex(memoryTypeIndex);
+
+        m_memory = allocator->device.allocateMemory(memoryInfo, nullptr, VkBlock::getDispatchLoader());
+        allocator->device.bindBufferMemory(m_buffer, m_memory, 0, VkBlock::getDispatchLoader());
+    }
+
+    void VkBlock::free(Allocator* allocator)
+    {
+        allocator->device.destroyBuffer(m_buffer, nullptr, VkBlock::getDispatchLoader());
+        allocator->device.freeMemory(m_memory, nullptr, VkBlock::getDispatchLoader());
+    }
+
+    uint64_t VkBlock::getVMA() { return (uint64_t)(VkDeviceMemory)(m_memory); }
+}
